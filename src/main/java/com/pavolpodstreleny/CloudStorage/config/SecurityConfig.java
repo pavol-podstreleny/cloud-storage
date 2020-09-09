738e5765e95e8ec7deb89a99ff17014830dba740
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-09-09 17:24:11 +0200

    Created Login View and Controller
    Added SecurityConfig for securing endpoints

---
+++ src/main/java/com/pavolpodstreleny/CloudStorage/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.pavolpodstreleny.CloudStorage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/signup", "/css/**", "/js/**").permitAll().anyRequest()
+                .authenticated();
+
+        http.formLogin().loginPage("/login").permitAll().and().formLogin().defaultSuccessUrl("/home", true);
+        http.logout().permitAll();
+    }
+}
